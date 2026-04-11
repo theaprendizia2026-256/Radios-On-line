2c2199179ccf0d594a3d85d9cdbd72cf5bad9eef
TheAprendiz.IA2026 <theaprendiz.ia2026@gmail.com>
2026-04-11 00:41:13 -0400

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ const STATIONS = [
     { name: "0R - Piano Classical, Berlin, Germany", frequency: 89.6, stream: "https://0nlineradio.radioho.st/classical-classical-piano?ref=radio-browser26" },
     { name: "0R - Bach Classical, Renania, Alemania", frequency: 89.8, stream: "https://0nlineradio.radioho.st/0r-bach?ref=radio-browser26" },
     { name: "100 Greatest Classical Music, 	Ontario, Canada", frequency: 90.1, stream: "https://az1.mediacp.eu/listen/100greatestclassicalmusic/radio.mp3" },  
-    { name: "Classical Music Network, Monterrey, México", frequency: 90.3, stream: "https://az1.mediacp.eu/listen/100greatestclassicalmusic/radio.mp3" },
+    { name: "Classical Music Network, Monterrey, México", frequency: 90.3, stream: "https://securestreams7.autopo.st/?uri=http://s3.voscast.com:10018/stream" },
     { name: "Pop Classical Music, Moscú, Rusia", frequency: 90.5, stream: "https://pub0202.101.ru:8443/stream/pro/aac/64/97" },
     { name: "Classical Calm by King FM, Seattle, Estados Unidos", frequency: 90.7, stream: "https://classicalking.streamguys1.com/evergreen-aac-128k" },
     { name: "Classical 90.3, Shanghái, China", frequency: 90.9, stream: "https://lhttp.qingting.fm/live/267/64k.mp3" },
