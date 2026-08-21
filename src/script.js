eb7a6f5f01cbd8c01d7506ec96cf4ead12b89a2a
TheAprendiz.IA2026 <theaprendiz.ia2026@gmail.com>
2026-08-21 12:15:17 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -22,8 +22,8 @@ const CATEGORIES_STATIONS = {
     { name: "Radio Movie Soundtracks Hits, EE.UU.", frequency: 98.1, stream: "https://strm112.1.fm/moviesoundtracks_mobile_mp3" },
   ],
   1: [ // Audiolibros
-    { name: "El Inamovible - Baldomero Lillo", frequency: "Cuento-Chileno", stream: "./src/audios.mp3/inamible.mp3"},
-    { name: "La Ajorca de Oro - Gustavo Adolfo Bécquer", frequency: "Leyenda-Española", stream: "./src/audios.mp3/ajorca.mp3" }
+    { name: "El Inamovible - Baldomero Lillo", frequency: "Cuento", stream: "./audios.mp3/inamible.mp3"},
+    { name: "La Ajorca de Oro - Gustavo Adolfo Bécquer", frequency: "Leyenda", stream: "./audios.mp3/ajorca.mp3" }
   ],
   2: [ // Noticias
     { name: "Radio Cooperativa, Chile", frequency: 98.8, stream: "https://unlimited3-cl.dps.live/cooperativafm/mp3/icecast.audio" },
