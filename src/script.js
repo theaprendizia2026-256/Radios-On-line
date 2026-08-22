75a830d40fc88988806d9f324289a5b8a8fc37f4
TheAprendiz.IA2026 <theaprendiz.ia2026@gmail.com>
2026-08-22 02:28:58 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -31,6 +31,7 @@ const CATEGORIES_STATIONS = {
   2: [ // Noticias
     { name: "Radio Cooperativa, Chile", frequency: 98.8, stream: "https://unlimited3-cl.dps.live/cooperativafm/mp3/icecast.audio" },
     { name: "Radio Bio Bio, Chile", frequency: 98.9, stream: "https://unlimited3-cl.dps.live/biobiosantiago/mp3/icecast.audio" },
+    { name: "Radio El Conquistador, Chile", frequency: 98.6, stream: "https://stream10.usastreams.com/9314/stream/" },
     { name: "RFI Español, Francia", frequency: 98.4, stream: "https://rfienespagnol64k.ice.infomaniak.ch/rfienespagnol-64.mp3" },
     { name: "RNE - Radio Nacional de España", frequency: 98.5, stream: "https://dispatcher.rndfnk.com/crtve/rne5/lcg/mp3/high" },
     { name: "Radio Sputnik, Moscow, Rusia", frequency: 98.7, stream: "https://icecast-rian.cdnvideo.ru/voicerus" },
@@ -40,17 +41,19 @@ const CATEGORIES_STATIONS = {
     { name: "RNE Radio 3, España", frequency: 96.9, stream: "https://rtvelivestream.rtve.es/rtvesec/rne/rne_r3_main.m3u8" },
   ],
   4: [ // Lo-Fi / Ambient
-    { name: "Radio SomaFM Groove Salad, SF, EE.UU", frequency: 96.2, stream: "https://ice5.somafm.com/groovesalad-128-mp3" },
-    { name: "Radio SomaFM Drone Zone, SF, EE.UU.", frequency: 96.5, stream: "https://ice5.somafm.com/dronezone-128-mp3" },
-    { name: "Radio Swiss Jazz, Basilea, Suiza", frequency: 96.8, stream: "https://stream.srg-ssr.ch/m/rsj/mp3_128" },
-    { name: "Adroit Jazz Underground, New York, EEUU", frequency: 96.2, stream: "https://icecast.walmradio.com:8443/jazz_opus" },
-    { name: "Bossa Jazz, Brasilea, Brasil", frequency: 96.8, stream: "https://centova5.transmissaodigital.com:20104/live" }, 
-    { name: "Sunset Chillout Lounge, New York, EEUU", frequency: 96.8, stream: "https://stream.srg-ssr.ch/m/rsj/mp3_128" },
+    { name: "Radio SomaFM Groove Salad, SF, EE.UU", frequency: 96.0, stream: "https://ice5.somafm.com/groovesalad-128-mp3" },
+    { name: "Radio SomaFM Drone Zone, SF, EE.UU.", frequency: 96.1, stream: "https://ice5.somafm.com/dronezone-128-mp3" },
+    { name: "Radio Swiss Jazz, Basilea, Suiza", frequency: 96.3, stream: "https://stream.srg-ssr.ch/m/rsj/mp3_128" },
+    { name: "Adroit Jazz Underground, New York, EEUU", frequency: 96.5, stream: "https://icecast.walmradio.com:8443/jazz_opus" },
+    { name: "Bossa Jazz, Brasilea, Brasil", frequency: 96.6, stream: "https://centova5.transmissaodigital.com:20104/live" }, 
+    { name: "Sunset Chillout Lounge, New York, EEUU", frequency: 96.7, stream: "https://stream.srg-ssr.ch/m/rsj/mp3_128" },
     { name: "Music for sleep, Berlin, Germany", frequency: 96.8, stream: "https://0nlineradio.radioho.st/classical-classical-music-for-sleep?ref=radio-browser26" },
   ],
   5: [ // Miscelánea
     { name: "Radio Always Elvis, Randers, Denmark", frequency: 97.3, stream: "https://radioserver.dk/alwayselvisradio" },
     { name: "Radio Italia anni 60, Milán, Italia", frequency: 97.1, stream: "https://ice12.fluidstream.net/ria60_mi.aac" },
+    { name: "Radio Addictive 50s, Toronto, Canada", frequency: 97.2, stream: "https://quincy.torontocast.com:2945/stream" },
+    { name: "Radio Oldies And Rock N Roll, EEUU", frequency: 97.5, stream: "https://listen.radioking.com/radio/382444/stream/433642" },
   ]
 };
 
